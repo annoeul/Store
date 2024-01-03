0cb8d285f7d89a6efeb078fc1e9ebb909972dd01
annoeul <shdmf1137@gmail.com>
2024-01-03 19:07:52 +0900

    commit

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -2,12 +2,14 @@ import React from "react"
 import { Route, Routes } from "react-router-dom"
 import LoginPage from "../pages/LoginPage"
 import RegisterPage from "../pages/SignupPage"
+import Home from "../pages/Home"
 
 function Router() {
   return (
     <Routes>
-      {/* <Route path="/" element={<LoginPage />} /> */}
-      <Route path="/" element={<RegisterPage />} />
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<RegisterPage />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   )
 }
